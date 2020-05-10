13ed43f36b4383f6ec929c996f84ad2a30c42cf2
Jp-Salgad <63624496+Jp-Salgad@users.noreply.github.com>
2020-05-10 15:23:33 -0300

    Update numero5.java

--- numero5.java
+++ numero5.java
@@ -9,61 +9,63 @@ public class numero5 {
 
 		int num;
 		float num1, resultado1, resultado2, resultado3, resultado4;
-		float preço1, preço2, preço3, preço4, vdd;
+		float preÃ§o1, preÃ§o2, preÃ§o3, preÃ§o4, vdd;
 		
 		System.out.println("==============================================================");
-		System.out.println("| Código |              Condição de Pagamento                |");
+		System.out.println("| CÃ³digo |              CondiÃ§Ã£o de Pagamento                |");
 		System.out.println("==============================================================");
-		System.out.println("|    1   | À vista em dinheiro ou cheque, com 10% de desconto|");
-		System.out.println("|    2   | À vista com cartão de crédito, com 5% de desconto |");
-		System.out.println("|    3   | Em 2 vezes, preço normal de etiqueta sem juros    |");
-		System.out.println("|    4   | Em 3 vezes, preço de etiqueta com acréscimo de 10%|");
+		System.out.println("|    1   | Ã€ vista em dinheiro ou cheque, com 10% de desconto|");
+		System.out.println("|    2   | Ã€ vista com cartÃ£o de crÃ©dito, com 5% de desconto |");
+		System.out.println("|    3   | Em 2 vezes, preÃ§o normal de etiqueta sem juros    |");
+		System.out.println("|    4   | Em 3 vezes, preÃ§o de etiqueta com acrÃ©scimo de 10%|");
 		System.out.println("==============================================================");
 		
-		System.out.println("Quanto que foi o produto que você comprou?");
+		System.out.println("Quanto que foi o produto que vocÃª comprou?");
 		num1 = leitor.nextFloat();	
         
 		
-		System.out.println("Escolha um código de condição de pagamento para efetuar a sua compra.");
+		System.out.println("Escolha um cÃ³digo de condiÃ§Ã£o de pagamento para efetuar a sua compra.");
 		num = leitor.nextInt();	
 		
 		switch(num) {
         
         case 1:
-        	System.out.println("Você escolheu a condição de pagamento à vista em dinheiro ou cheque, com 10% de desconto");
+        	System.out.println("VocÃª escolheu a condiÃ§Ã£o de pagamento Ã  vista em dinheiro ou cheque, com 10% de desconto");
         resultado1 = (float) (num1 * 10)/100;
-        preço1 = (float) (num1 - resultado1);
+        preÃ§o1 = (float) (num1 - resultado1);
         System.out.println("Compra realizada com sucesso!");
-        System.out.println("O valor final da sua compra é: R$" +preço1);
+        System.out.println("O valor final da sua compra Ã©: R$" +preÃ§o1);
         
         break;
         
         case 2:
-        	System.out.println("Você escolheu a condição de pagamento à vista com cartão de crédito, com 5% de desconto");
+        	System.out.println("VocÃª escolheu a condiÃ§Ã£o de pagamento Ã  vista com cartÃ£o de crÃ©dito, com 5% de desconto");
         resultado2 = (float) (num1 * 5)/100;
-        preço2 = (float) (num1 - resultado2);
+        preÃ§o2 = (float) (num1 - resultado2);
         System.out.println("Compra realizada com sucesso!");
-        System.out.println("O valor final da sua compra é: R$" +preço2);
+        System.out.println("O valor final da sua compra Ã©: R$" +preÃ§o2);
         
         break;
         
         case 3:
-        	System.out.println("Você escolheu a condição de pagamento em 2 vezes, preço normal de etiqueta sem juros");
-        preço3 = (float) (num1/2);
-        System.out.println("Você irá pagar, sem juros, duas parcelas de: R$" +preço3);
+        	System.out.println("VocÃª escolheu a condiÃ§Ã£o de pagamento em 2 vezes, preÃ§o normal de etiqueta sem juros");
+        preÃ§o3 = (float) (num1/2);
+        System.out.println("VocÃª irÃ¡ pagar, sem juros, duas parcelas de: R$" +preÃ§o3);
         
         break;
         
         case 4:
-        	System.out.println("Você escolheu a condição de pagamento em 3 vezes, preço de etiqueta com acréscimo de 10%");
+        	System.out.println("VocÃª escolheu a condiÃ§Ã£o de pagamento em 3 vezes, preÃ§o de etiqueta com acrÃ©scimo de 10%");
         resultado4 = (float) (num1 * 10)/100;
-        preço4 = (float) (num1 + resultado4);
-        vdd = (float) (preço4/3);
+        preÃ§o4 = (float) (num1 + resultado4);
+        vdd = (float) (preÃ§o4/3);
         
-        System.out.println("Você irá pagar, com acréscimo de 10%, 3 parcelas de: R$" +vdd);
+        System.out.println("VocÃª irÃ¡ pagar, com acrÃ©scimo de 10%, 3 parcelas de: R$" +vdd);
         
-        default:
-        	System.out.println("Não existe condição de pagamento para esse código. Por favor, digite novamente o código.");
+        break;
+			
+	default:
+        	System.out.println("NÃ£o existe condiÃ§Ã£o de pagamento para esse cÃ³digo. Por favor, digite novamente o cÃ³digo.");
         	
         	
         	
